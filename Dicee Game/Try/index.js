66b8916adc26c50adf3ab8a372fa6115e11eb1f7
Sense <mehershakti1020@gmail.com>
2023-04-08 17:17:33 +0530

    Dice Game modified

---
+++ Dicee Game/Try/index.js
@@ -0,0 +1,45 @@
+
+
+//for player changing the dice images
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImages = "dice"+ randomNumber1 + ".png";
+
+var ramdomImageSource = "images/" + randomDiceImages; 
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", ramdomImageSource);
+
+// 🏉🏉🏉🏉🏉🏉🏉short hand type🏉🏉🏉🏉🏉🏉🏉
+// document.querySelectorAll("img")[0].setAttribute("src", "images/dice" + (Math.floor(Math.random()*6)+1) + ".png");
+// 🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉
+
+
+// 🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉
+//for player changing the dice images
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var ramdomImageSource2 = "images/dice"+ randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", ramdomImageSource2);
+
+
+// 🏉🏉🏉🏉🏉🏉🏉short hand type🏉🏉🏉🏉🏉🏉🏉
+//changing the dice images for player 2 using the random accouding to the number
+// document.querySelectorAll("img")[1].setAttribute("src", "images/dice" + (Math.floor(Math.random()*6)+1) +".png")
+// 🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉
+
+
+if (randomNumber1 > randomNumber2) 
+{
+    document.querySelector("h1").innerHTML = "🏆 Player1 Won ";    
+}
+else if (randomNumber1 < randomNumber2) 
+{
+    document.querySelector("h1").innerHTML = "Player 2 won 🏆";
+}
+else
+{
+    document.querySelector("h1").innerHTML = "🏆 Draw 🏆";
+}
